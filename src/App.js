d4c6a62594b033d1afb2cc2a5a8d172dc1047ef5
RashmikaTR <tharusharashmika2001@gmail.com>
2026-03-17 13:16:13 +0530

    update login.jsx on app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Products from "./pages/Products";
+import "./pages/Login"
+import Login from "./pages/Login";
+
 
 function App() {
   return (
@@ -12,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
 
     </Router>
